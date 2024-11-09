a53024bf2de0b58d1c30fb7c08a3175502f8e8db
Jace <contactjacenair@gmail.com>
2024-11-09 22:22:16 +0000

    Create a channel and register a signal

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"micro-service-app/handlers"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -26,6 +29,40 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	// Bind address to the newly creater servemux server
-	http.ListenAndServe("192.168.122.17:9090", sm)
+	// Manually configuring the server
+	s := &http.Server{
+		Addr:         "192.168.122.17:9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// Handl Listen and Serve in go func to avoid blocking the service
+	go func() {
+		// Listen and serve
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// Use os signal package to register notification of certian signals
+	// Create a channel to recieve a signal
+	sigChan := make(chan os.Signal)
+	// Broadcast a message on this channel wheneven an operating system (os) Interrupt or Kill command is received
+	signal.Notify(sigChan, os.Interrupt)
+	// Register for signal
+	signal.Notify(sigChan, os.Kill)
+
+	// Reading from a channel will block until there's a message available to be consumed
+	sig := <-sigChan
+	l.Println("Recieved terminate, graceful shutdown", sig)
+
+	// Once the message is consumed, create a context for graceful shutdown. tc is time context
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	// Graceful server shutdown with tc
+	s.Shutdown(tc)
+
 }
